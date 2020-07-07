73eeca1a5eb46be36836679b60f4986ab5225fb3
Zack <enders.zack@gmail.com>
2020-07-07 01:10:59 -0400

    Add card shuffle. Fixed multicard submisson on the front end, but still need to handle the websocket side. Start work on cookie functionality. Refactored code.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState, useReducer } from 'react';
-import data from '../data/test';
 import './App.css';
-import 'keen-slider/keen-slider.min.css';
 
-const URL = 'ws://localhost:8080';
-const ws = new WebSocket(URL);
+const URL = 'ws://192.168.1.221:8080';
+const ws = new WebSocket(URL, sessionStorage.getItem('CAH_USER_ID')); // Update to cookie later, ss is good for testing
 
 const App = () => {
+    const [cardsPlayed, setCardsPlayed] = useState([]);
     const reducer = (state, action) => {
         console.log('action ', action);
         switch (action.type) {
+            case 'returnUser':
+                return { game: action.payload.game, player: action.payload.player };
             case 'userInit':
+                sessionStorage.setItem('CAH_USER_ID', action.payload.playerID);
                 return { ...state, player: action.payload };
             case 'startGame':
                 return {
@@ -41,6 +43,7 @@ const App = () => {
                 return { ...state, player: { ...state.player, whiteCards: action.payload } };
             case 'nextPlayer':
                 console.log(`Black Card: ${action.payload.winner.blackCard}, \n White Card: ${action.payload.winner.whiteCards.map((card) => card)}`);
+                setCardsPlayed([]);
                 return {
                     ...state,
                     game: {
@@ -62,6 +65,7 @@ const App = () => {
         }
     };
     const [state, dispatch] = useReducer(reducer, { game: { isActive: false }, player: null });
+
     console.log('state ', state);
 
     useEffect(() => {
@@ -74,18 +78,25 @@ const App = () => {
             // on receiving a message, add it to the list of messages
             const message = JSON.parse(evt.data);
             const { type, payload } = message;
-            dispatch({
-                type,
-                payload,
-            });
+
+            if (type === 'checkUser') {
+                return ws.send(
+                    JSON.stringify({
+                        type: 'verifyUser',
+                        payload: sessionStorage.getItem('CAH_USER_ID'),
+                    })
+                );
+            } else {
+                dispatch({
+                    type,
+                    payload,
+                });
+            }
         };
 
         ws.onclose = () => {
             console.log('disconnected');
-            // automatically try to reconnect on connection loss
-            // setState({
-            //     ws: new WebSocket(URL),
-            // });
+            // write something to try and reconnect every x for x seconds
         };
     }, []);
 
@@ -98,18 +109,26 @@ const App = () => {
         );
     };
 
-    const playWhiteCards = (index) => {
-        if (state.game.inPlayCards.blackCard.pick === 1) {
-            ws.send(
-                JSON.stringify({
-                    type: 'playCard',
-                    payload: {
-                        playerID: state.player.playerID,
-                        cards: [index],
-                    },
-                })
-            );
+    // Is this the best way to do this? Seems meh compared to the callback feature on this.setState
+    useEffect(() => {
+        if (state.game.isActive && cardsPlayed.length && state.game.playerTurn !== state.player.playerID) {
+            console.log('cardsPlayed ', cardsPlayed);
+            if (cardsPlayed.length === state.game.inPlayCards.blackCard.pick) {
+                ws.send(
+                    JSON.stringify({
+                        type: 'playCard',
+                        payload: {
+                            playerID: state.player.playerID,
+                            cards: cardsPlayed,
+                        },
+                    })
+                );
+            }
         }
+    }, [cardsPlayed]);
+
+    const playWhiteCards = (index) => {
+        setCardsPlayed([...cardsPlayed, ...[index]]);
     };
 
     const selectWinner = (player) => {
@@ -124,6 +143,30 @@ const App = () => {
                 },
             })
         );
+    };
+
+    const showOverlay = () => {
+        if (state.player.playerID === state.game.playerTurn) {
+            return (
+                <div className="overlay" style={{ display: 'flex' }}>
+                    <h2 className="overlay__player-turn">It's your turn to choose the best white card!</h2>
+                </div>
+            );
+        } else if (cardsPlayed.length >= state.game.inPlayCards.blackCard.pick) {
+            return (
+                <div className="overlay" style={{ display: 'flex' }}>
+                    <h2 className="overlay__played">You already selected your {cardsPlayed.length > 1 ? 'cards' : 'card'}</h2>
+                </div>
+            );
+        } else if (state.game.inPlayCards.whiteCards.filter((player) => player.playerID === state.player.playerID)[0]) {
+            return (
+                <div className="overlay" style={{ display: 'flex' }}>
+                    <h2 className="overlay__played">You already selected your {cardsPlayed.length > 1 ? 'cards' : 'card'}</h2>
+                </div>
+            );
+        } else {
+            return <div className="overlay" style={{ display: 'none' }}></div>;
+        }
     };
 
     if (!state.game.isActive) {
@@ -142,10 +185,10 @@ const App = () => {
                     <div className="black-card-container">
                         <div className="card card--black">{state.game.inPlayCards.blackCard.text}</div>
                     </div>
-                    <div>
+                    {/* <div>
                         <h3>Chat</h3>
                         <div>Chat here yay!</div>
-                    </div>
+                    </div> */}
                 </div>
                 <div className="cards">
                     <div className="cards__in-play">
@@ -164,13 +207,16 @@ const App = () => {
                     </div>
                     <div className="cards__in-hand">
                         <h3>Your cards</h3>
-                        <ul className="card__grid card__grid--player">
-                            {state.player.whiteCards.map((card, index) => (
-                                <li style={state.player.playerID === state.game.playerTurn ? { backgroundColor: 'red' } : { backgroundColor: 'white' }} key={card} className="card card--white" onClick={() => playWhiteCards(index)}>
-                                    <span dangerouslySetInnerHTML={{ __html: card }}></span>
-                                </li>
-                            ))}
-                        </ul>
+                        <div className="card__container">
+                            <ul className="card__grid card__grid--player">
+                                {state.player.whiteCards.map((card, index) => (
+                                    <li key={card} className="card card--white" onClick={() => playWhiteCards(index)}>
+                                        <span dangerouslySetInnerHTML={{ __html: card }}></span>
+                                    </li>
+                                ))}
+                            </ul>
+                            {showOverlay()}
+                        </div>
                     </div>
                 </div>
             </div>
--- ws-server/index.js
+++ ws-server/index.js
@@ -1,14 +1,18 @@
 const WebSocket = require('ws');
 const data = require('./data');
 
+// TODO:
+// - Write out a cookie that saves the user id so if a user disconnects they can come back in with the cards the had before hand
+// - Make sure game data display's when a user comes back from a dc or refresh
+// - Figure out how to hand 2 white cards for the double black cards
+// - Write function that randomly picks cards instead of pulling the first 7 - DONE : Decks shuffle at the start of the game
+// - Make it so host can choose decks to play with
+// - Make a ready check feature with the start game button
+// - Make users add a username
+
 const server = () => {
     const wss = new WebSocket.Server({ port: 8080 });
     const cardData = { ...data };
-
-    const addGameCards = () => {
-        gameCards.whiteCards = cardData.whiteCards;
-        gameCards.blackCards = cardData.blackCards;
-    };
 
     // Generates unique ID for every new connection
     const getUniqueID = () => {
@@ -19,10 +23,6 @@ const server = () => {
         return s4() + s4() + '-' + s4();
     };
 
-    const getCard = () => {
-        return cardData.whiteCards.splice(0, 1)[0];
-    };
-
     let users = {};
 
     const gameCards = {
@@ -31,6 +31,7 @@ const server = () => {
     };
 
     let game = {
+        isActive: false,
         playerTurn: '',
         inPlayCards: {
             blackCard: '',
@@ -38,6 +39,74 @@ const server = () => {
         },
     };
 
+    /**
+     * Updates our game data so we can keep track of the cards currently in play
+     * @param {String} playerTurn - Person who is choosing the best white card
+     * @param {String} blackCard - Black card that is currently in play
+     * @param {Array} whiteCards - White cards that are currently in play
+     */
+    const updateGame = (playerTurn, blackCard, whiteCards) => {
+        game = {
+            isActive: true, // Updating the game should always make the game active?
+            playerTurn: playerTurn,
+            inPlayCards: {
+                blackCard: blackCard,
+                whiteCards: whiteCards,
+            },
+        };
+    };
+
+    /**
+     *  Randomly picks cards from the correct deck, depending on the action passed
+     *  - 'getBlackCard' : Single Black Card
+     *  - 'startGameWhiteCards' : 7* White Cards to start the game
+     *  - 'getWhiteCard' : Single White Card
+     * @param {String} action - Action that decides what cards to return
+     */
+    const getCards = (action) => {
+        switch (action) {
+            case 'startGameWhiteCards':
+                return gameCards.whiteCards.splice(0, 7);
+            case 'getBlackCard':
+                return gameCards.blackCards.splice(0, 1)[0];
+            case 'getWhiteCard':
+                return gameCards.whiteCards.splice(0, 1)[0];
+            default:
+                throw new Error();
+        }
+    };
+    /**
+     * Fisher-Yates Shuffle - Randomly shuffles the white and black cards when the game starts
+     * @param {*} array
+     */
+    const shuffleDeck = (array) => {
+        let m = array.length,
+            t,
+            i;
+
+        // While there remain elements to shuffle…
+        while (m) {
+            // Pick a remaining element…
+            i = Math.floor(Math.random() * m--);
+
+            // And swap it with the current element.
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
+
+        return array;
+    };
+
+    const addGameCards = () => {
+        gameCards.whiteCards = shuffleDeck(cardData.whiteCards);
+        gameCards.blackCards = shuffleDeck(cardData.blackCards);
+    };
+
+    /**
+     * Builds the user object
+     * @param {String} userID
+     */
     const buildUserObject = (userID) => {
         return {
             playerID: userID,
@@ -48,93 +117,105 @@ const server = () => {
         };
     };
 
-    wss.on('connection', function connection(ws, request, client) {
-        const userID = getUniqueID();
+    wss.on('connection', function connection(ws) {
+        const OLD_USER_ID = ws.protocol;
+        const userID = users[OLD_USER_ID] ? OLD_USER_ID : getUniqueID();
+        console.log('userID ', userID);
+
         ws.id = userID;
         ws.isAlive = true;
-        users[userID] = buildUserObject(userID);
 
-        ws.send(
-            JSON.stringify({
-                type: 'userInit',
-                payload: users[userID],
-            })
-        );
+        if (users[OLD_USER_ID] === undefined) {
+            console.log("User didn't exist, build user.");
+            users[userID] = buildUserObject(userID);
+            ws.send(
+                JSON.stringify({
+                    type: 'userInit',
+                    payload: users[userID],
+                })
+            );
+        } else {
+            const data = {
+                game,
+                player: users[userID],
+            };
+            console.log('game ', game.inPlayCards.whiteCards);
+            console.log('data ', data);
+            ws.send(
+                JSON.stringify({
+                    type: 'returnUser',
+                    payload: data,
+                })
+            );
+        }
 
         ws.on('message', function incoming(data) {
+            const USER_IDS = Object.keys(users);
             const { type, payload } = JSON.parse(data);
+
             switch (type) {
                 case 'startGame':
                     addGameCards();
 
-                    Object.keys(users).map((user) => {
-                        users[user].whiteCards = gameCards.whiteCards.splice(0, 7);
+                    USER_IDS.map((user) => {
+                        users[user].whiteCards = getCards('startGameWhiteCards');
                     });
 
-                    const startingBlackCard = gameCards.blackCards.splice(0, 1)[0];
+                    const startingBlackCard = getCards('getBlackCard');
+                    const startingPlayerTurn = USER_IDS.reduce((acc, user) => {
+                        if (users[user].host === true) acc = users[user].playerID;
+                        return acc;
+                    }, '');
 
-                    game = {
-                        playerTurn: Object.keys(users).reduce((acc, user) => {
-                            if (users[user].host === true) acc = users[user].playerID;
-                            return acc;
-                        }, ''),
-                        inPlayCards: {
-                            blackCard: startingBlackCard,
-                            whiteCards: [],
-                        },
-                    };
+                    updateGame(startingPlayerTurn, startingBlackCard, []);
 
                     wss.clients.forEach(function each(client) {
                         if (client.readyState === WebSocket.OPEN) {
-                            const data = {
-                                type: 'startGame',
-                                payload: {
-                                    playerTurn: Object.keys(users).reduce((acc, user) => {
-                                        if (users[user].host === true) acc = users[user].playerID;
-                                        return acc;
-                                    }, ''),
-                                    blackCard: startingBlackCard,
-                                    whiteCards: users[client.id].whiteCards,
-                                },
-                            };
-                            client.send(JSON.stringify(data));
+                            client.send(
+                                JSON.stringify({
+                                    type: 'startGame',
+                                    payload: {
+                                        playerTurn: startingPlayerTurn,
+                                        blackCard: startingBlackCard,
+                                        whiteCards: users[client.id].whiteCards,
+                                    },
+                                })
+                            );
                         }
                     });
 
                     break;
                 case 'playCard':
                     // Keep our game date updated so if someone disconnects we can use it again
-                    game = {
-                        playerTurn: game.playerTurn,
-                        inPlayCards: {
-                            blackCard: game.inPlayCards.blackCard,
-                            whiteCards: [...game.inPlayCards.whiteCards, ...payload.cards.map((idx) => users[payload.playerID].whiteCards[idx])],
-                        },
-                    };
+
+                    const whiteCardsPlayed = payload.cards.map((idx) => users[payload.playerID].whiteCards[idx]);
+
+                    updateGame(game.playerTurn, game.inPlayCards.blackCard, [...game.inPlayCards.whiteCards, { playerID: payload.playerID, cards: whiteCardsPlayed }]);
 
                     wss.clients.forEach(function each(client) {
                         if (client.readyState === WebSocket.OPEN) {
-                            const data = {
-                                type: 'whitePlayed',
-                                payload: {
-                                    whiteCards: [
-                                        {
-                                            playerID: payload.playerID,
-                                            cards: payload.cards.map((idx) => users[payload.playerID].whiteCards[idx]),
-                                        },
-                                    ],
-                                },
-                            };
-                            client.send(JSON.stringify(data));
+                            client.send(
+                                JSON.stringify({
+                                    type: 'whitePlayed',
+                                    payload: {
+                                        whiteCards: [
+                                            {
+                                                playerID: payload.playerID,
+                                                cards: whiteCardsPlayed,
+                                            },
+                                        ],
+                                    },
+                                })
+                            );
                         }
                     });
 
                     payload.cards.forEach((card) => {
                         delete users[payload.playerID].whiteCards[card];
-                        const newCard = getCard();
-                        users[payload.playerID].whiteCards[card] = newCard;
+                        users[payload.playerID].whiteCards[card] = getCards('getWhiteCard');
                     });
 
+                    // Send new white cards to the client that played the white cards
                     ws.send(
                         JSON.stringify({
                             type: 'whiteCardUpdate',
@@ -144,22 +225,16 @@ const server = () => {
 
                     break;
                 case 'selectWinner':
-                    const currentUser = Object.keys(users).indexOf(payload.playerTurn);
-                    console.log('currentUser ', currentUser);
-                    const nextUser = Object.keys(users)[currentUser + 1] || Object.keys(users)[0];
-                    console.log('nextUser ', nextUser);
+                    const currentUser = USER_IDS.indexOf(payload.playerTurn);
+                    const nextUser = USER_IDS[currentUser + 1] || USER_IDS[0];
 
-                    game = {
-                        playerTurn: nextUser,
-                        inPlayCards: {
-                            blackCard: gameCards.blackCards.splice(0, 1)[0],
-                            whiteCards: [],
-                        },
-                    };
+                    updateGame(nextUser, getCards('getBlackCard'), []);
 
+                    // Update the winner player object with the black card that they won
                     users[payload.playerID].blackCards = [...users[payload.playerID].blackCards, payload.blackCard.text];
 
                     wss.clients.forEach(function each(client) {
+                        // Send new game data and previous round winning cards
                         if (client.readyState === WebSocket.OPEN) {
                             client.send(
                                 JSON.stringify({
@@ -175,6 +250,7 @@ const server = () => {
                             );
                         }
 
+                        // Send winner their black card
                         if (client.id === payload.playerID && client.readyState === WebSocket.OPEN) {
                             client.send(
                                 JSON.stringify({
@@ -200,10 +276,8 @@ const server = () => {
                     return user;
                 }
             });
-            console.log('usersLeft ', usersLeft);
 
             if (!usersLeft.length) {
-                users = {};
                 wss.close();
             }
         });
